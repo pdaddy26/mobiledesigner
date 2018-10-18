77f6b288f3a3476acccc6a1554841cab3b2faeef
Hong-Paul Pham <paul@Hong-Pauls-MacBook-Pro.local>
2018-10-18 07:45:19 -0400

    add designer and prop components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DesignerComponent } from './designer/designer.component';
+import { PropertiesComponent } from './properties/properties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesignerComponent,
+    PropertiesComponent
   ],
   imports: [
     BrowserModule

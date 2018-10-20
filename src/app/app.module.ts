a41df6f9d396cd47af5c1d8d36983c1fd52960c9
Hong-Paul Pham <paul@Hong-Pauls-MacBook-Pro.local>
2018-10-20 01:35:27 -0400

    add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,31 @@ import { DesignerComponent } from './components/designer/designer.component';
 import { PropertiesComponent } from './components/properties/properties.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     DesignerComponent,
-    PropertiesComponent
+    PropertiesComponent,
+    NavigationMenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatListModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
